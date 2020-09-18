6dcc0d241b756df9e44015ec0bc94cd25e3be513
MTaylorfullStack <mtaylor@codingdojo.com>
2020-09-17 20:04:35 -0500

    floodfill

--- week_three/recursion.js
+++ week_three/recursion.js
@@ -1,5 +1,24 @@
 // Recursion Overview
 
+// write a function that logs a greeting N number of times (accept N as a parameter)
+
+// write a function accepting N
+// console.log some greeting
+// if N is 0
+    // break out of the function
+    // return true
+// else
+    // invoke the function, passing in N-1
+
+function recursiveGreeting(N){
+    console.log("Hello! Welcome to recursion!")
+    if(N == 0 || N<0){
+        return true;
+    } else {
+        recursiveGreeting(N-1);
+    }
+}
+recursiveGreeting(10)
 
 
 // Flood Fill
@@ -12,9 +31,52 @@
 
 // Given a 2D canvas of: 
 
+
+// Our 2D Canvas becomes
+
+// canvas = [ [3,2,1,4,3],
+//            [2,1,1,4,0],
+//            [7,1,1,5,3],
+//            [6,5,1,4,1],
+//            [1,2,1,1,1] ]
+
+// declare a function, accepting three parameters; canvas, newColor, start
+// store originalColor = color at start
+// change the pixel at start to newColor
+// if pixel to the right is the same as original color && the pixel to the right is in bounds
+    // call the function, canvas, newColor, start, pixel to the rights coordinates as start
+// if pixel to the left is the same as original color && the pixel to the right is in bounds
+    // call the function; canvas, newColor, left as start 
+// if pixel to the top is the same as original color && the pixel to the top is in bounds
+    //  call the function; canvas, newColor, top as start
+// if pixel to the bottom is the same as original color && the pixel to the bottom is in bounds
+    //  call the function; canvas, newColor, bottom as start
+
+function floodFill(canvas, newColor, start){
+    var originalColor = canvas[start[0]][start[1]];
+    canvas[start[0]][start[1]] = newColor;
+    // Checking right pixel
+    if(canvas[start[0]][start[1]+1] == originalColor && start[1]+1 < canvas[0].length-1){
+        return floodFill(canvas, newColor, [start[0], start[1]+1]);
+    }
+    // Checking left pixel
+    if(canvas[start[0]][start[1]-1] == originalColor && start[1]-1 >= 0){
+        return floodFill(canvas, newColor, [start[0], start[1]-1]);
+    }
+    // Checking above pixel
+    if(canvas[start[0]-1][start[1]] == originalColor && start[0]-1 > 0){
+        return floodFill(canvas, newColor, [start[0]-1, start[1]]);
+    }
+    // Checking lower pixel
+    if(canvas[start[0]+1][start[1]] == originalColor && start[0]+1 < canvas.length-1){
+        return floodFill(canvas, newColor, [start[0]+1, start[1]]);
+    }
+    return canvas
+}
+
 canvas = [ [3,2,3,4,3],
            [2,3,3,4,0],
-           [7,3,3,5,3],
+           [7,3,3,1,3],
            [6,5,3,4,1],
            [1,2,3,3,3] ]
 
@@ -26,10 +88,4 @@ start = [2,2];
 
 color = 1;
 
-// Our 2D Canvas becomes
-
-canvas = [ [3,2,1,4,3],
-           [2,1,1,4,0],
-           [7,1,1,5,3],
-           [6,5,1,4,1],
-           [1,2,1,1,1] ]
+console.log(floodFill(canvas, color, start))
